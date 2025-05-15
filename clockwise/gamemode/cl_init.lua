b1255341c7b789e5286d96bd37c163f5f3e03f6b
modular442 <modular442@gmail.com>
2025-05-15 21:04:02 +0300

    ⬆️ Обновление gamemode/ директории

--- clockwise/gamemode/cl_init.lua
+++ clockwise/gamemode/cl_init.lua
@@ -2,13 +2,26 @@
     © 2025 modular442 (Modular Content). All rights reserved. 
     Something that you will never be. --]]
 
-local isLoad = Clockwise and true
-if isLoad then
-    print('[Clockwise] fsr')
+local startTime = os.clock()
+local msgColor = Color(0, 255, 100, 255)
+
+local isReload = Clockwise and true
+if isReload then
+	MsgC(msgColor, "[Clockwise] Change detected! Refreshing...\n")
+	table.Merge(Clockwise, GM)
 else
-    print('[Clockwise] fsl')
+	MsgC(msgColor, "[Clockwise] Framework is initializing...\n")
     ---@class Clockwise: GM
-    Clockwise = GM
+	Clockwise = GM
 end
 
 include('clockwise/framework/_clockwise.lua')
+
+if isReload then
+	Clockwise:Initialize()
+	MsgC(msgColor, "[Clockwise] Client-side AutoRefresh handled in " .. math.Round(os.clock() - startTime, 3) .. " second(s)\n")
+else
+	MsgC(msgColor, "[Clockwise] Framework loading took " .. math.Round(os.clock() - startTime, 3) .. " second(s)\n")
+end
+
+table.Merge(GM, Clockwise)
--- clockwise/gamemode/init.lua
+++ clockwise/gamemode/init.lua
@@ -2,15 +2,29 @@
     © 2025 modular442 (Modular Content). All rights reserved. 
     Something that you will never be. --]]
 
-local isLoad = Clockwise and true
-if isLoad then
-    print('[Clockwise] fsr')
+local startTime = os.clock()
+local msgColor = Color(0, 255, 100, 255)
+
+local isReload = Clockwise and true
+if isReload then
+	MsgC(msgColor, '[Clockwise] Change detected! Refreshing...\n')
+	table.Merge(Clockwise, GM)
 else
-    print('[Clockwise] fsl')
+	MsgC(msgColor, '[Clockwise] Framework is initializing...\n')
     ---@class Clockwise: GM
-    Clockwise = GM
+	Clockwise = GM
 end
+
+include('clockwise/framework/_clockwise.lua')
 
 AddCSLuaFile('cl_init.lua')
 AddCSLuaFile('clockwise/framework/_clockwise.lua')
-include('clockwise/framework/_clockwise.lua')
+
+if isReload then
+	Clockwise:Initialize()
+	MsgC(msgColor, '[Clockwise] Server-side AutoRefresh handled in ' .. math.Round(os.clock() - startTime, 3) .. ' second(s)\n')
+else
+	MsgC(msgColor, '[Clockwise] Framework loading took ' .. math.Round(os.clock() - startTime, 3) .. ' second(s)\n')
+end
+
+table.Merge(GM, Clockwise)
